708742ded54a95b7ad2541a51a53e5f51337018d
kinaterme <jakubchloupek14@gmail.com>
2025-11-23 05:17:27 +0100

    arstarst

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using Realms;
 
 namespace OsuSkinReader
 {
-
     public class Skin : RealmObject
     {
         [PrimaryKey]
         public Guid ID { get; set; }
 
         public string Name { get; set; }
-
         public IList<RealmNamedFileUsage> Files { get; }
     }
 
     public class RealmNamedFileUsage : RealmObject
     {
-      public File? File { get; set; }
-      public string? Filename { get; set; }
+        public File? File { get; set; }
+        public string? Filename { get; set; }
     }
 
     public class File : RealmObject
@@ -31,7 +30,18 @@ namespace OsuSkinReader
     {
         static void Main(string[] args)
         {
-            string dbPath = $"/home/{Environment.UserName}/.local/share/osu/client.realm";
+            string GetLazerPath()
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local/share/osu");
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "osu");
+                else
+                    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library/Application Support/osu");
+            }
+            
+            string lazerPath = GetLazerPath();
+            string dbPath = Path.Combine(lazerPath, "client.realm");
             var config = new RealmConfiguration(dbPath)
             {
                 IsReadOnly = true,
@@ -43,20 +53,22 @@ namespace OsuSkinReader
                     typeof(RealmNamedFileUsage)
                 }
             };
-
             var realm = Realm.GetInstance(config);
 
-            Console.WriteLine("Skins in the database:");
+            var skinFiles = new List<(string SkinName, Guid SkinID, string Filename, string? Hash)>();
             foreach (var skin in realm.All<Skin>())
             {
-                Console.WriteLine($"Skin: {skin.Name} ({skin.ID})");
-
+                //Console.WriteLine($"Skin: {skin.Name} ({skin.ID})");
                 foreach (var usage in skin.Files)
                 {
-                    Console.WriteLine($" File: {usage.Filename} => Hash: {usage.File?.Hash}");
+                    if (usage.Filename.Contains("default-") || usage.Filename.Contains("hit0") || usage.Filename.Contains("hit50") || usage.Filename.Contains("hit100") || usage.Filename.Contains("hit300") || usage.Filename.Contains("hitcircle.png"))
+                        skinFiles.Add((skin.Name, skin.ID, usage.Filename, usage.File?.Hash));
+                    //Console.WriteLine($" File: {usage.Filename} => Hash: {usage.File?.Hash}");
                 }
             }
+
+            foreach (var file in skinFiles)
+                Console.WriteLine($"{file.SkinName} ({file.SkinID}) | {file.Filename} | {file.Hash}");
         }
     }
 }
-
